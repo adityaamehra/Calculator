db7a4a2f7f3b1f96dd228ddedd29f88bba805ca4
Adityaa Mehra <adirakash@gmail.com>
2021-07-04 09:40:46 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Adityaa Mehra on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
